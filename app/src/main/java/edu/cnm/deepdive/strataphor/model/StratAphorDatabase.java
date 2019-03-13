dc1b89f813fb2d52efc08312c73f05934cdc2da7
Lance Zotigh <lancezotighlz@gmail.com>
2019-03-13 16:04:25 -0600

    Added database callback for preload.

--- app/src/main/java/edu/cnm/deepdive/strataphor/model/StratAphorDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/strataphor/model/StratAphorDatabase.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive.strataphor.model;
 
+import static javafx.scene.input.KeyCode.R;
+
 import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
@@ -8,13 +10,11 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import edu.cnm.deepdive.android.BaseFluentAsyncTask;
-import edu.cnm.deepdive.strataphor.R;
 import edu.cnm.deepdive.strataphor.StratAphorApplication;
 import edu.cnm.deepdive.strataphor.model.dao.SayingDao;
 import edu.cnm.deepdive.strataphor.model.dao.SourceDao;
 import edu.cnm.deepdive.strataphor.model.entity.Saying;
 import edu.cnm.deepdive.strataphor.model.entity.Source;
-import edu.cnm.deepdive.strataphor.service.RandomSaying;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -48,8 +48,82 @@ public abstract class StratAphorDatabase extends RoomDatabase {
     private static final StratAphorDatabase INSTANCE =
         Room.databaseBuilder(StratAphorApplication.getInstance().getApplicationContext(),
             StratAphorDatabase.class, DATABASE_NAME)
+            .addCallback(new Callback())
             .build();
 
   }
 
+  private static class Callback extends RoomDatabase.Callback {
+
+    @Override
+    public void onCreate(@NonNull SupportSQLiteDatabase db) {
+      super.onCreate(db);
+      new PreloadTask()
+          .setSuccessListener((sayings) -> {
+            //TODO Do something with sayings so we can use them
+          })
+          .execute();
+    }
+
+  }
+
+  private static class PreloadTask
+      extends BaseFluentAsyncTask<Void, Void, List<Saying>, List<Saying>> {
+
+    @Nullable
+    @Override
+    protected List<Saying> perform(Void... voids) throws TaskException {
+      Context context = StratAphorApplication.getInstance().getApplicationContext();
+      StratAphorDatabase database = StratAphorDatabase.getInstance();
+      try (
+          InputStream input = context.getResources().openRawResource(R.raw.sources);
+          Reader reader = new InputStreamReader(input);
+          CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT);
+      ) {
+        List<Saying> sayings = new LinkedList<>();
+        for (CSVRecord record : parser) {
+          Source source = new Source();
+          source.setName(record.get(0));
+          String resourceName = record.get(1);
+          long sourceId = database.getSourceDao().insert(source);
+          sayings.addAll(loadSayings(sourceId, resourceName));
+        }
+          database.getSayingDao().insert(sayings);
+          return database.getSayingDao().findAll();
+      } catch (IOException e) {
+        throw new TaskException(e);
+      }
+    }
+
+    private List<Saying>loadSayings(long sourceId, String resourceName) {
+      Context context = StratAphorApplication.getInstance().getApplicationContext();
+      int resourceId =
+          context.getResources().getIdentifier(resourceName, "raw", context.getPackageName());
+      try(InputStream input = context.getResources().openRawResource(resourceId);
+        Reader reader = new InputStreamReader(input);
+        BufferedReader buffer = new BufferedReader(reader);
+        ){
+          List<Saying> sayings = new LinkedList<>();
+          String line;
+          while ((line = buffer.readLine()) != null) {
+            if (!(line = line.trim()).isEmpty()) {
+              Saying saying = new Saying();
+              saying.setSourceId(sourceId);
+              saying.setText(line);
+              sayings.add(saying);
+            }
+          }
+          return sayings;
+      } catch (IOException e) {
+        throw  new TaskException(e);
+      }
+    }
+
+  }
+
 }
+
+
+
+
+
